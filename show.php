8b051d4fd84cdc48b8124db1e82f9aad126e26e6
it63070160 <71114745+it63070160@users.noreply.github.com>
2020-11-15 21:34:52 +0700

    Update show.php

--- show.php
+++ show.php
@@ -1,44 +1,47 @@
 <html>
-<head>
-<title>ITF Lab</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</head>
-<body>
-<?php
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'itflab13.mysql.database.azure.com', 'it63070160@itflab13', 'ITFlab13', 'itflab', 3306);
-if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-$res = mysqli_query($conn, 'SELECT * FROM guestbook');
-?>
-<table width="600" border="1">
-  <tr class="table-primary">
-    <th width="100"> <div align="center">Name</div></th>
-    <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Action </div></th>
-  </tr>
-<?php
-while($Result = mysqli_fetch_array($res))
-{
-?>
-  <tr class="table-info">
-    <td><?php echo $Result['Name'];?></td>
-    <td><?php echo $Result['Comment'];?></td>
-    <td><a href="delete.php?id=<?php echo $Result['ID']; ?>" class="btn btn-danger">ลบ</a> <a href="update.php?id=<?php echo $Result["ID"]; ?>" class="btn btn-warning" >แก้ไข</a></td>
-  </tr>
-<?php
-}
-?>
-</table>
-<?php
-mysqli_close($conn);
-?>
-</body>
+  <head>
+    <title>ITF Lab</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <?php
+    $conn = mysqli_init();
+    mysqli_real_connect($conn, 'itflab13.mysql.database.azure.com', 'it63070160@itflab13', 'ITFlab13', 'itflab', 3306);
+    if (mysqli_connect_errno($conn))
+    {
+        die('Failed to connect to MySQL: '.mysqli_connect_error());
+    }
+    $res = mysqli_query($conn, 'SELECT * FROM guestbook');
+    ?>
+    <div class="container">
+    <table class="table table-stripped table-bordered table-hover">
+      <tr class="table-primary">
+        <th width="100"> <div align="center">Name</div></th>
+        <th width="350"> <div align="center">Comment </div></th>
+        <th width="150"> <div align="center">Action </div></th>
+      </tr>
+      <?php
+      while($Result = mysqli_fetch_array($res))
+      {
+      ?>
+        <tr class="table-info">
+          <td><?php echo $Result['Name'];?></td>
+          <td><?php echo $Result['Comment'];?></td>
+          <td><a href="delete.php?id=<?php echo $Result['ID']; ?>" class="btn btn-danger">ลบ</a><a href="update.php?id=<?php echo $Result["ID"]; ?>" class="btn btn-warning" >แก้ไข</a></td>
+        </tr>
+      <?php
+      }
+      ?>
+    <a href="form.html" class="btn btn-success">เพิ่ม +</a>
+    </table>
+    </div>
+    <?php
+    mysqli_close($conn);
+    ?>
+  </body>
 </html>
