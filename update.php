112aa9bc0580e176cb48ebe21ccf9eb268bec280
it63070160 <71114745+it63070160@users.noreply.github.com>
2020-11-15 23:34:03 +0700

    Update update.php

--- update.php
+++ update.php
@@ -16,73 +16,29 @@
     {
         die('Failed to connect to MySQL: '.mysqli_connect_error());
     }
-    if (isset($_REQUEST['id'])) {
-      try {
-        $id = $_REQUEST['id'];
-        $res = mysqli_query($conn, "SELECT * FROM guestbook WHERE id='$id'");
-        extract($res);
-      }
-    }
-
-    if (isset($_REQUEST['upbtn'])) {
-      $name_up = $_REQUEST['name']
-      $comment_up = $_REQUEST['comment']
-      $link_up = $_REQUEST['link']
-      if (empty($name_up)) {
-        $errorMsg = 'Please Enter Firstname';
-      } else if (empty($comment_up)) {
-        $errorMsg = "Please Enter Comment ";
-      } else {
-        try {
-          if (!isset($errorMsg)) {
-            $update_stmt = "UPDATE guestbook SET Name='$name_up', Comment='$comment_up', Link='$link_up' WHERE id='$id'"
-            if (mysqli_query($conn, $sql)) {
-              $updateMsg = "New record updated successfully";
-              header("refresh:2;show.php");
-            }
-          }
-        }
-      }
-    }
+    $res = mysqli_query($conn, "SELECT * FROM guestbook WHERE id='$id'");
     ?>
     <div class="container">
     <div class="display-3 text-center mb-3">Edit Form</div>
-
-    <?php
-      if (isset($errorMsg)) {
-    ?>
-      <div class="alert alert-danger">
-        <strong>Wrong! <?php echo $errorMsg; ?></strong>
-      </div>
-    <?php } ?>
-
-    <?php
-      if (isset($updateMsg)) {
-    ?>
-      <div class="alert alert-success">
-        <strong>Success! <?php echo $updateMsg; ?></strong>
-      </div>
-    <?php } ?>
-
       <form method = "post" id="update" class="form-horizontal mt-5">
         <div class="form-group">
           <label for="name" class="col-sm-3 control-label">Name</label>
           <div class="col-sm-6">
-            <input type="text" name="name" id="idname" class="form-control" value="<?php echo $name; ?>">
+            <input type="text" name="name" id="idname" class="form-control" value="<?php echo $res['Name']; ?>">
           </div>
         </div>
 
         <div class="form-group">
           <label for="comment" class="col-sm-3 control-label">Comment</label>
           <div class="col-sm-6">
-            <textarea rows="10" cols="40" name = "comment" id="idComment" value="<?php echo $comment; ?>"></textarea>
+            <textarea rows="10" cols="40" name = "comment" id="idComment" value="<?php echo $res['Comment']; ?>"></textarea>
           </div>
         </div>
 
         <div class="form-group">
           <label for="link" class="col-sm-3 control-label">Link</label>
           <div class="col-sm-6">
-            <input type="text" name="link" id="idlink" class="form-control" value="<?php echo $link; ?>">
+            <input type="text" name="link" id="idlink" class="form-control" value="<?php echo $res['Link']; ?>">
           </div>
         </div>
 
