07fbee45612727e1da0b0413e73c5df748e66805
it63070160 <71114745+it63070160@users.noreply.github.com>
2020-11-16 00:44:41 +0700

    Update and rename edit.php to delete.php

--- delete.php
+++ delete.php
@@ -7,18 +7,15 @@ if (mysqli_connect_errno($conn))
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
-$id = $_POST['ID'];
-$name = $_POST['name'];
-$comment = $_POST['comment'];
-$link = $_POST['link'];
+$id = $_REQUEST['delete_id'];
 
 
-$sql = "UPDATE guestbook SET Name='$name', Comment='$comment', Link='$link' WHERE id='$id'";
+$sql = "DELETE FROM guestbook WHERE id='$id'";
 
 
 if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
-    header("refresh:2;show.php");
+    echo "Record deleted successfully";
+    header("Location:show.php");
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
