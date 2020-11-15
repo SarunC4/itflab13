b71860a88a06d1417c08fc75a96f180a47085f03
it63070160 <71114745+it63070160@users.noreply.github.com>
2020-11-16 00:52:00 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -18,7 +18,7 @@ $sql = "INSERT INTO guestbook (Name, Comment, Link) VALUES ('$name', '$comment',
 
 if (mysqli_query($conn, $sql)) {
     echo "New record created successfully";
-    header("refresh:2;show.php");
+    ?><a href="show.php" class="btn btn-warning">กลับ</a><?php
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
