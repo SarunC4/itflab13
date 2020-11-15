fbec75d2990f4ed553ec4fa3119d80afe39c5cb7
it63070160 <71114745+it63070160@users.noreply.github.com>
2020-11-16 01:35:52 +0700

    Update updatedb.php

--- updatedb.php
+++ updatedb.php
@@ -1,27 +1,41 @@
-<?php
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Added</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <?php
 
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'itflab13.mysql.database.azure.com', 'it63070160@itflab13', 'ITFlab13', 'itflab', 3306);
-if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
+    $conn = mysqli_init();
+    mysqli_real_connect($conn, 'itflab13.mysql.database.azure.com', 'it63070160@itflab13', 'ITFlab13', 'itflab', 3306);
+    if (mysqli_connect_errno($conn))
+    {
+        die('Failed to connect to MySQL: '.mysqli_connect_error());
+    }
 
-$edit_id = $_GET['edit_id'];
-$name = $_POST['name'];
-$comment = $_POST['comment'];
-$link = $_POST['link'];
-
-
-$sql = "UPDATE guestbook SET Name='$name', Comment='$comment', Link='$link' WHERE id='$edit_id'";
+    $edit_id = $_GET['edit_id'];
+    $name = $_GET['name'];
+    $comment = $_GET['comment'];
+    $link = $_GET['link'];
 
 
-if (mysqli_query($conn, $sql)) {
-    ?><div class="display-3 text-center mb-3">Updated Successfully</div>
-    <div class="text-center"><a href="show.php" class="btn btn-warning">กลับ</a></div><?php
-  } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-  }
-  
-mysqli_close($conn);
-?>
+    $sql = "UPDATE guestbook SET Name='$name', Comment='$comment', Link='$link' WHERE id='$edit_id'";
+
+
+    if (mysqli_query($conn, $sql)) {
+        ?><div class="display-3 text-center mb-3">Updated Successfully</div>
+        <div class="text-center"><a href="show.php" class="btn btn-warning">กลับ</a></div><?php
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+    
+    mysqli_close($conn);
+    ?>
+  </body>
+</html>
